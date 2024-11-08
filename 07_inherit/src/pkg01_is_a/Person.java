736eab4769afbc62703a4afac630f2976d0dd41e
김성율 <ojh3998@naver.com>
2024-11-08 13:14:20 +0900

    20241108

---
+++ 07_inherit/src/pkg01_is_a/Person.java
@@ -0,0 +1,22 @@
+package pkg01_is_a;
+
+/*
+┌--------------┐
+│    Person    │ 슈퍼 클래스
+│--------------│
+│     eat()    │
+│    sleep()   │
+└--------------┘
+*/
+
+public class Person {
+  
+  public void eat() {
+    System.out.println("eat");
+  }  
+    
+  public void sleep() {  
+    System.out.println("sleep");
+  }
+
+}
